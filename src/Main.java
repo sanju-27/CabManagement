36fb0383c9f2958e2270acd3aa1c8b1043c5ed9a
sanju-27 <sanjaystz98@gmail.com>
2019-06-21 16:28:51 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
-import jdk.internal.dynalink.beans.StaticClass;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
     static int rideId = 0;
+
     public static void main(String[] args) throws IOException {
         Admin admin = new Admin();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,16 +12,14 @@ public class Main {
         do {
             System.out.println("0.Admin\n1.Driver\n2.Customer\n3.Exit\nEnter choice...");
             lch = Integer.parseInt(br.readLine());
-            switch(lch)
-            {
+            switch (lch) {
                 case 0:
                     System.out.println("Welcome Admin");
                     int ch1;
                     do {
                         System.out.println("1. View Cabs\n2.View Customer\n3.New Cab\n4.New Location\n5.View Locations\n6.Exit");
                         ch1 = Integer.parseInt(br.readLine());
-                        switch (ch1)
-                        {
+                        switch (ch1) {
                             case 1:
                                 admin.viewCabs();
                                 break;
@@ -39,33 +36,30 @@ public class Main {
                                 admin.viewLoc();
                                 break;
                         }
-                    } while (ch1<6);
+                    } while (ch1 < 6);
                     break;
                 case 1:
                     Drivers d = admin.dLogin();
-                    if(d!=null)
-                    {
-                        System.out.println("Welcome "+d.name);
+                    if (d != null) {
+                        System.out.println("Welcome " + d.name);
                         d.viewHistory();
                     }
                     break;
                 case 2:
                     Customer c;
                     System.out.println("1.Existing\n2.New User");
-                    ch1 = Integer.parseInt(br.readLine());int id;
-                    if(ch1==2)
-                    {
+                    ch1 = Integer.parseInt(br.readLine());
+                    int id;
+                    if (ch1 == 2) {
                         c = admin.newCust();
-                    }
-                    else {
+                    } else {
                         System.out.println("Enter your ID");
                         id = Integer.parseInt(br.readLine());
                         c = admin.customer.get(id - 1);
                         System.out.println("Enter Password");
-                        if(br.readLine().equals(c.getPwd()))
+                        if (br.readLine().equals(c.getPwd()))
                             System.out.println("Success...");
-                        else
-                        {
+                        else {
                             System.out.println("Password Incorrect");
                             break;
                         }
@@ -76,13 +70,12 @@ public class Main {
                         ch2 = Integer.parseInt(br.readLine());
                         if (ch2 == 1) {
                             admin.hail(c, rideId++);
-                        }
-                        else if(ch2==2)
+                        } else if (ch2 == 2)
                             c.viewHistory();
-                    }while(ch2<3);
+                    } while (ch2 < 3);
 
             }
 
-        }while(lch<3);
+        } while (lch < 3);
     }
 }
